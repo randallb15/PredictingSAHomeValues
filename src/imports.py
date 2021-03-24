c374fa284a8a9aa41e4dfac5604bcbad32303efd
Randall Bloomquist <randallb2015@gmail.com>
2021-03-23 22:07:49 -0500

    updates to GBR model

--- src/imports.py
+++ src/imports.py
@@ -21,7 +21,7 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, SCORERS
 from sklearn.metrics import precision_recall_curve, roc_curve, confusion_matrix, classification_report,precision_score,recall_score,accuracy_score,roc_auc_score, f1_score
 from sklearn.utils import resample
 from sklearn.utils.class_weight import compute_class_weight
